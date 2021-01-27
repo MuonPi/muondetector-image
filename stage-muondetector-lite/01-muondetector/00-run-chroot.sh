105d012ba427b08166c8cc0ba7cf20866c6fb4fc
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-01-27 02:19:03 +0100

    Changed 00-run-chroot.sh script for lihght image creation

--- stage-muondetector-lite/01-muondetector/00-run-chroot.sh
+++ stage-muondetector-lite/01-muondetector/00-run-chroot.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-touch muondetector.gpg
-wget -qO - https://archive.muonpi.org/muondetector.public.key | gpg --no-default-keyring --keyring ./muondetector.gpg --import -
-mv muondetector.gpg /etc/apt/trusted.gpg.d/
+wget -qO - https://archive.muonpi.org/muondetector.public.key | sudo apt-key add -
 
-echo deb https://archive.muonpi.org/raspbian buster main > /etc/apt/sources.list.d/muondetector.list
+echo "deb https://archive.muonpi.org/raspbian buster main" > /etc/apt/sources.list.d/muondetector.list
 
 apt update
