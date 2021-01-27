022067b1cdad30b57f4bee6913086fb5991eb35b
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-01-27 02:23:12 +0100

    Changed configuration to install the proper packages also for the gui

--- stage-muondetector-gui/01-muondetector/00-run-chroot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-touch muondetector.gpg
-wget -qO - https://archive.muonpi.org/muondetector.public.key | gpg --no-default-keyring --keyring ./muondetector.gpg --import -
-mv muondetector.gpg /etc/apt/trusted.gpg.d/
-
-echo deb https://archive.muonpi.org/raspbian buster main > /etc/apt/sources.list.d/muondetector.list
-
-apt update
