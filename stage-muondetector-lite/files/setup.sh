cb5b5097502d178aeb01864b88165f26a18f0c5c
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-01-29 15:09:22 +0100

    Improved first setup script

--- stage-muondetector-lite/files/setup.sh
+++ stage-muondetector-lite/files/setup.sh
@@ -5,6 +5,18 @@ echo "For this first setup, please set a new password for the pi user:"
 
 sudo passwd pi
 
+echo "Testing internet connection..."
+ping -c 2 muonpi.org &> /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "No internet connection detected."
+    echo "I will launch a shell for you to establish your internet connection now."
+    echo "When you are done and have verified that it works, exit the shell."
+    /bin/bash
+else
+    echo "Established."
+fi
+
 echo "Now enter your mqtt credentials for the muonpi network:"
 
 muondetector-login
