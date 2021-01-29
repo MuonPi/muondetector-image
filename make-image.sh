fd8ccccb18965126b885d1a569ef5450c7593fd9
Daniel Treffenstädt <52167116+dtreffenstaedt@users.noreply.github.com>
2021-01-29 12:41:31 +0100

    Update make-image.sh

--- make-image.sh
+++ make-image.sh
@@ -10,14 +10,6 @@ fi
 
 echo "using config $whichconfig"
 
-echo "Trying to start binfmt-support service..."
-systemctl start binfmt-support.service
-
-if [ $? -gt 0 ]; then
-    echo "Please make sure you have binfmt-support installed on your system." 
-    exit 1
-fi
-
 if [ -e pi-gen ]; then
     rm -rf pi-gen
 fi
