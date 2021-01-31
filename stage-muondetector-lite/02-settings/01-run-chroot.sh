d4d72be30e06ccdc371303d10df9024700c07b5d
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-01-31 16:12:00 +0100

    Improved sed command to remove serial console from commandline. Now matches all baud rates and serial0,ttyAMA0 and ttyS0

--- stage-muondetector-lite/02-settings/01-run-chroot.sh
+++ stage-muondetector-lite/02-settings/01-run-chroot.sh
@@ -10,5 +10,5 @@ raspi-config nonint do_serial 0
 #dtparam=i2c_arm=on
 raspi-config nonint do_i2c 0
 #disable serial login
-sed -i 's/console\=serial0,115200*//' /boot/cmdline.txt
+sed -i -e 's/\( *\)console=\(serial\|tty\(AMA\|S\)\)0,[0-9]* */\1/' /boot/cmdline.txt
 
