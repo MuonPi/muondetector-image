207b2760b92919e9e8820a46e2b0c97ee56ef915
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-02-03 10:35:52 +0100

    Added serial port selector for raspberry pi 3

--- stage-muondetector-lite/02-settings/01-run-chroot.sh
+++ stage-muondetector-lite/02-settings/01-run-chroot.sh
@@ -12,3 +12,5 @@ raspi-config nonint do_i2c 0
 #disable serial login
 sed -i -e 's/\( *\)console=\(serial\|tty\(AMA\|S\)\)0,[0-9]* */\1/' /boot/cmdline.txt
 
+# Set the serial port selector for paspberry pi 3
+echo -e "[pi3]\ndtoverlay=pi3-miniuart-bt" >> /boot/config.txt
