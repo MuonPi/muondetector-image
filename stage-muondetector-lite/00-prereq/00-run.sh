a2842104eb31470dbfd05f2c0e7cd7532d564a0c
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-02-03 10:40:57 +0100

    Changed script which installs the motd message to use the new filename

--- stage-muondetector-lite/00-prereq/00-run.sh
+++ stage-muondetector-lite/00-prereq/00-run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm "${ROOTFS_DIR}/etc/motd"
-install files/11-muonpiwelcome "${ROOTFS_DIR}/etc/update-motd.d/"
+install files/20-muonpi-motd "${ROOTFS_DIR}/etc/update-motd.d/"
 install files/setup.sh "${ROOTFS_DIR}/home/pi/.bash_profile"
