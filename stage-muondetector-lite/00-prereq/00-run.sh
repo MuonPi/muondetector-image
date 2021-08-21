35cec5170c1907c46c28b257c410f4dcfb4466a5
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-08-21 16:10:23 +0200

    Fixed initial setup script location and call

--- stage-muondetector-lite/00-prereq/00-run.sh
+++ stage-muondetector-lite/00-prereq/00-run.sh
@@ -3,8 +3,10 @@
 rm "${ROOTFS_DIR}/etc/motd"
 install files/20-muonpi-motd "${ROOTFS_DIR}/etc/update-motd.d/"
 install files/zprofile "${ROOTFS_DIR}/home/pi/.zprofile"
+install -d "${ROOTFS_DIR}/opt/muondetector/"
+install files/initial_setup "${ROOTFS_DIR}/opt/muondetector/initial_setup"
 install files/zshrc "${ROOTFS_DIR}/home/pi/.zshrc"
-install -d files/zshrc.d "${ROOTFS_DIR}/home/pi/.zshrc.d"
+install -d "${ROOTFS_DIR}/home/pi/.zshrc.d"
 for f in $(ls files/zshrc.d/); do
     install "files/zshrc.d/$f" "${ROOTFS_DIR}/home/pi/.zshrc.d/"
 done
