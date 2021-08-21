b4c009b9f5270dd928b187642369a4aee285e400
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-08-21 15:59:28 +0200

    Renamed ~/.profile to ~/.zprofile

--- stage-muondetector-lite/00-prereq/00-run.sh
+++ stage-muondetector-lite/00-prereq/00-run.sh
@@ -2,6 +2,6 @@
 
 rm "${ROOTFS_DIR}/etc/motd"
 install files/20-muonpi-motd "${ROOTFS_DIR}/etc/update-motd.d/"
-install files/sprofile "${ROOTFS_DIR}/home/pi/.profile"
+install files/zprofile "${ROOTFS_DIR}/home/pi/.zprofile"
 install files/zshrc "${ROOTFS_DIR}/home/pi/.zshrc"
 install files/zshrc.d "${ROOTFS_DIR}/home/pi/.zshrc.d"
